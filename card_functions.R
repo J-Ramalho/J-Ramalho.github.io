6aad7409cb2622bb32a54973ad69476f385c318a
Ramalho <Joao.Ramalho@rdor.nestle.com>
2020-09-17 09:52:52 +0200

    Converted the site into a personnal site. Added page with books with automatic loading from a list that has the images paths. This was a project from longtime ago!

---
+++ card_functions.R
@@ -0,0 +1,36 @@
+library("dplyr")
+library("htmltools")
+
+get_title_panel <- function() {
+  h1("Amazon Inc.",
+     tags$br(),
+     tags$small("2019 Annual Report",
+                class = "text-muted text-primary"))
+}
+
+exec_card_component <- function(name, title, portrait) {
+  div(
+    class = "card",
+    img(class = "card-img-top",
+        src = portrait,
+        alt = name,
+        width = "100"),
+    div(
+      class = "card-body",
+      h5(class = "card-title",
+         name),
+      p(class = "card-text",
+        title)
+    )
+  )
+}
+
+get_cards_component <- function(data) {
+  data %>%
+    purrr::pmap(exec_card_component) %>%
+    div(class = "card-wrapper") %>%
+    doRenderTags()
+}
+
+
+
--- spc_raw.R
+++
@@ -1,8 +0,0 @@
-bamako_long <- bamako %>% 
-  mutate(hour_num = str_extract(Hour, "[:digit:]+")) %>%
-  pivot_longer(cols = names(bamako)[str_which(names(bamako), "[Sample]")],
-               names_to = "sample",
-               values_to = "weight"
-  ) %>%
-  mutate(sample_num = str_extract(sample, "[:digit:]+")) %>%
-  select(sample_num, weight)
