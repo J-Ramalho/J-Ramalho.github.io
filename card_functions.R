a283d1264bbab40e3d74e6fdff22af39fdae2217
Ramalho <Joao.Ramalho@rdor.nestle.com>
2020-09-17 22:22:41 +0200

    simplifying image rendering. added additional pages to the web site.

--- card_functions.R
+++ card_functions.R
@@ -2,9 +2,9 @@ library("dplyr")
 library("htmltools")
 
 get_title_panel <- function() {
-  h1("Amazon Inc.",
+  h1("Science Fiction",
      tags$br(),
-     tags$small("2019 Annual Report",
+     tags$small("Novels to open the mind",
                 class = "text-muted text-primary"))
 }
 
@@ -14,15 +14,17 @@ exec_card_component <- function(name, title, portrait) {
     img(class = "card-img-top",
         src = portrait,
         alt = name,
-        width = "100"),
+        width = "200"),
     div(
       class = "card-body",
       h5(class = "card-title",
          name),
       p(class = "card-text",
         title)
-    )
+    ),
+    tags$br()  
   )
+  
 }
 
 get_cards_component <- function(data) {
