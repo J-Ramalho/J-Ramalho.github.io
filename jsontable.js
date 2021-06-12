ad764f0a71ec7c8b3c47c6487fe7ba4f30a8896c
J-Ramalho <ramalho.joao@protonmail.com>
2021-06-12 23:03:33 +0200

    javascript for books introduced

---
+++ jsontable.js
@@ -0,0 +1,18 @@
+fetch('data/scifi.json')
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    appendData(data);
+})
+.catch(function (err) {
+    console.log('error: ' + err);
+});
+function appendData(data) {
+var mainContainer = document.getElementById("myData");
+for (var i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = ' ' + data[i].firstName + ' ' + data[i].lastName + ' ' + data[i].title;
+    mainContainer.appendChild(div);
+}
+}
