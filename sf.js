1c0881c46374e2a8a381ba58e3b9b2b6e93754ed
J-Ramalho <ramalho.joao@protonmail.com>
2021-06-13 12:47:44 +0200

    completed table structure and added review

--- sf.js
+++ sf.js
@@ -1,4 +1,4 @@
-fetch('data/scifi.json')
+fetch('data/sf.json')
 .then(function (response) {
     return response.json();
 })
@@ -10,34 +10,38 @@ fetch('data/scifi.json')
 });
 
 function createTable(data) {
-    // Create a table header:
-    var table = document.createElement("table");
+    // Create a table:
+    var sfTable = document.createElement("table");
     // Create table headers:
-    var header = table.createTHead();
+    var header = sfTable.createTHead();
     var row = header.insertRow(0);
     var cell1 = row.insertCell(0);
     cell1.innerHTML = "<b>First Name</b>";
     var cell2 = row.insertCell(1);
     cell2.innerHTML = "<b>Family Name</b>";
-    var cell3 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
     cell3.innerHTML = "<b>Title</b>";
+    var cell4 = row.insertCell(3);
+    cell4.innerHTML = "<b>Review</b>";
     // Select DOM element that will be modified:
-    var mainContainer = document.getElementById("sfData");
-    // Populate cells with json file content:
+    var sfDiv = document.getElementById("sfDiv");
+    // Populate table cells with json file content and append to DOM element:
     for (var i = 0; i < data.length; i++) {
         // Create an empty <tr> element and add it to the 1st position of the table:
-        var row = table.insertRow(1);
+        var row = sfTable.insertRow(1);
         // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);  
         var cell3 = row.insertCell(2);  
-        // Add some text to the new cells:
+        var cell4 = row.insertCell(3);
+        // Add text from json to the new cells:
         cell1.innerHTML = data[i].firstName;
         cell2.innerHTML = data[i].lastName; 
         cell3.innerHTML = data[i].title; 
+        cell4.innerHTML = data[i].review;
         // Update the DOM element with new data:
-        mainContainer.appendChild(table);
-        // Assigning a css class to the created object
-        mainContainer.className = "booklist"
+        sfDiv.appendChild(sfTable);        
     }
+    // Assign a css class to the created object
+    sfDiv.className = "booklist"
 }
