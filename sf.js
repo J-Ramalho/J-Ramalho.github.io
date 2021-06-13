221da11f319d9d58e477cb542e6bf0cb53cae5ca
J-Ramalho <ramalho.joao@protonmail.com>
2021-06-13 13:42:03 +0200

    completed table including images, ready to style and populate

--- sf.js
+++ sf.js
@@ -16,29 +16,40 @@ function createTable(data) {
     var header = sfTable.createTHead();
     var row = header.insertRow(0);
     var cell1 = row.insertCell(0);
-    cell1.innerHTML = "<b>First Name</b>";
+    cell1.innerHTML = "<b>Cover</b>";
     var cell2 = row.insertCell(1);
-    cell2.innerHTML = "<b>Family Name</b>";
+    cell2.innerHTML = "<b>Title</b>";
     var cell3 = row.insertCell(2);
-    cell3.innerHTML = "<b>Title</b>";
+    cell3.innerHTML = "<b>Given</b>";
     var cell4 = row.insertCell(3);
-    cell4.innerHTML = "<b>Review</b>";
+    cell4.innerHTML = "<b>Name</b>";
+    var cell5 = row.insertCell(4);
+    cell5.innerHTML = "<b>Review</b>"
     // Select DOM element that will be modified:
     var sfDiv = document.getElementById("sfDiv");
     // Populate table cells with json file content and append to DOM element:
     for (var i = 0; i < data.length; i++) {
         // Create an empty <tr> element and add it to the 1st position of the table:
         var row = sfTable.insertRow(1);
-        // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+        // Insert new cells (<td> elements) in the "new" <tr> element:
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);  
         var cell3 = row.insertCell(2);  
         var cell4 = row.insertCell(3);
-        // Add text from json to the new cells:
-        cell1.innerHTML = data[i].firstName;
-        cell2.innerHTML = data[i].lastName; 
-        cell3.innerHTML = data[i].title; 
-        cell4.innerHTML = data[i].review;
+        var cell5 = row.insertCell(4);
+        // Add book cover images to the table cells :
+        var coverImage = document.createElement("IMG")
+        coverImage.setAttribute("src", data[i].cover);
+        coverImage.setAttribute("width", "80");
+        coverImage.setAttribute("height", "130");
+        cell1.innerHTML = coverImage;
+        cell1.innerHTML = ""; //to remove [objecthtmlimageelement], see references
+        cell1.appendChild(coverImage);
+        // Add text from json to the table cells:
+        cell2.innerHTML = data[i].title;
+        cell3.innerHTML = data[i].firstName; 
+        cell4.innerHTML = data[i].lastName; 
+        cell5.innerHTML = data[i].review;
         // Update the DOM element with new data:
         sfDiv.appendChild(sfTable);        
     }
